c8b5ad387cd4f4d4d593e4ed140f3239ac9ec330
E <127677871+Great3github@users.noreply.github.com>
2025-08-06 12:53:36 -0400

    fix3

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -62,9 +62,12 @@ function App({ user }: { user: { email: string; username: string; displayName: s
     room,
     onMessage: (evt) => {
       const message = JSON.parse(evt.data as string) as Message;
-      if (user.displayName === "") {alert("Blank display names are not allowed!")}
+      if (user.displayName === "") {
+        alert("Blank display names are not allowed!")
+        window.close()
+      }
       if (message.type === "add") {
-        if (user.displayName === "") {window.close()}
+        
         const foundIndex = messages.findIndex((m) => m.id === message.id);
         if (foundIndex === -1) {
           // probably someone else who added a message
