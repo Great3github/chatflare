d39ceb2486a5e7a509115a34444a05c942503a55
E <127677871+Great3github@users.noreply.github.com>
2025-08-25 13:56:13 -0400

    idk what the fuck i'm doing

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -28,7 +28,7 @@ export default function Login({ onLogin }: LoginProps) {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onLogin({ email, roomname, displayName });
-    navigate(`/${roomname}`); // Go to room URL dynamically
+    //navigate(`/${roomname}`); // Go to room URL dynamically
   };
 
   return (
@@ -49,7 +49,9 @@ export default function Login({ onLogin }: LoginProps) {
     </form>
   );
 }
-
+function doNothingFunction() {
+        // This function does nothing
+}
 function RootApp() {
   const [user, setUser] = useState<{
     email: string;
@@ -69,7 +71,8 @@ function RootApp() {
           user ? (
             <App user={user} />
           ) : (
-            <Navigate to="/" />
+            //<Navigate to="/" />
+            doNothingFunction()
           )
         }
       />
